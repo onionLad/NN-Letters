50ba7e6df7aa59b45e6dcfc7e7421cc61a18a369
Bill <bill.xia.pa@gmail.com>
2023-05-24 20:30:47 -0400

    Data isn't being corrupted by preprocessing

---
+++ verify_txts.py
@@ -0,0 +1,23 @@
+#
+# verify_txts.py
+#   Bill Xia
+#   5/24/23
+#
+# This file reads in a txt file created using an image file and confirms that
+# the txt file is an accurate representation of the image file.
+#
+
+# Impmorts
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Body - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+sample = pd.read_csv('data/Img_txts/img011-001.csv').values.flatten()
+sample = np.atleast_2d(np.append(sample, [1])).reshape(21,28)
+print(sample)
+plt.imshow(sample)
+plt.show()
+
